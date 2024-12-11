c5d8588e154ff21c79fc04c519b55c9f14a9edf7
Lore Nyanski <lorenyanski@gmail.com>
2024-12-11 16:53:43 +0100

    Can send messages (proper intent implementation needed)

--- bot.py
+++ bot.py
@@ -7,13 +7,22 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-
-client = discord.Client(intents=discord.Intents.default())
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
     print(f'{client.user} has logged in\n\nConnected to the following guilds:')
     for guild in client.guilds:
         print(f'    {guild.name} (id: {guild.id})')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == 'door':
+        await message.channel.send("look at this dumbass XD")
+
 
 client.run(TOKEN)
