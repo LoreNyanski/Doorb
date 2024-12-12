2ec037aeb98a34487c9ced362fe223416dd77fb2
Lore Nyanski <lorenyanski@gmail.com>
2024-12-12 18:16:48 +0100

    Amongus detector

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import discord
+import re
 
 from dotenv import load_dotenv
 
@@ -20,9 +21,23 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    
+
+    # test run
     if message.content == 'door':
         await message.channel.send("look at this dumbass XD")
 
+
+    # Amongus detector
+    if re.search('.*a.*m.*o.*g.*u.*s.*', message.content):
+        msg = message.content
+        response = []
+        for i in 'amogus':
+            indx = msg.find(i)
+            response.append(msg[:indx])
+            response.append(msg[indx])
+            msg = msg[min(indx+1,len(msg)):]
+        response.append(msg)
+
+        await message.channel.send('|| ' + '||'.join(response) + ' ||')
 
 client.run(TOKEN)
