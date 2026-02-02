e0dc92c5e6aa924371955ed706d24254f66e76a2
Lore <lorenyanski@gmail.com>
2026-02-02 08:24:21 +0100

    run script

---
+++ run.sh
@@ -0,0 +1,35 @@
+#! bin/bash
+
+# find process
+# kill it
+# pull from github
+# run the bot
+
+# ----- vars ----
+SCRIPT_DIR = "$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_MAIN = "bot_main.py"
+SCRIPT_VENV = ".venv"
+LOG_FILE = app.log
+PID_FILE = app.pid
+
+cd "$SCRIPT_DIR"
+
+# ----- find & kill process -----
+if [[ -f "$PID_FILE" ]]; then
+    OLD_PID = "$(cat "$PID_FILE")"
+    if kill -0 "$OLD_PID" 2>/dev/null; then
+        kill "$OLD_PID" 
+        sleep 2
+    fi
+fi
+
+# ----- pull from github -----
+git fetch origin
+git reset --hard origin/main
+
+# ----- updating requirements -----
+"$SCRIPT_VENV/bin/pip" install -r requirements.txt
+
+# ----- run the bot -----
+nohup "$SCRIPT_VENV/bin/python" "$SCRIPT_MAIN" > "$LOG_FILE" 2>&1 &
+echo $! > "$PID_FILE"
