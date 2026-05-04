4cbcdbf33e72bf836dda566dc07ab4b16f0bc8bc
Lore <lorenyanski@gmail.com>
2026-05-04 13:42:19 +0200

    Update env.py

--- src/env.py
+++ src/env.py
@@ -7,9 +7,3 @@ load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 OPEN_AI_TOKEN = os.getenv("OPEN_AI_TOKEN")
-
-shibe = int(os.getenv("shibe"))
-lore = int(os.getenv("lore"))
-
-test_guild = int(os.getenv("test_guild"))
-sticker = int(os.getenv("tracked_sticker"))
